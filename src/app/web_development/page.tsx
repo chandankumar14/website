1e23fb2c72506d3e3df53fd74962d09e422ceb07
chandankumar1307 <chandan.kumar@elitemindz.co>
2024-08-05 17:55:28 +0530

    web development ui changed

--- src/app/web_development/page.tsx
+++ src/app/web_development/page.tsx
@@ -1,72 +1,142 @@
-"use client"
-import OurValuePage from "@/components/home_page_content/our_value";
-import Image from "next/image"
-const Frontend = require("../../../public/frontEnd.svg")
-const backend = require("../../../public/backend DEvelopment.svg")
-const ui_ux = require("../../../public/UIUx.jpg")
+"use client";
+import Image from "next/image";
+const Frontend = require("../../../public/frontEnd.png");
+const backend = require("../../../public/backend.png");
+const ui_ux = require("../../../public/UIUx.jpg");
+const bgImage = require("../../../public/webDevelopment.png");
 import "./web.css";
 export default function WebDevelopmentPage() {
-    return (
-        <>
-            <main className="py-20 mb-4  banner-section">
-                <p className="items-center text-center py-36 text-cyan-50">
-                    From dynamic web applications to scalable backend services,
-                    Our web development expertise ensures high-performance solutions
-                    that deliver seamless user experiences, demonstrating our commitment
-                    to leveraging the web's full potential for business growth.
+  return (
+    <>
+      <section>
+        <div className="relative font-sans before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
+          <Image
+            src={bgImage}
+            alt="Banner Image"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+
+          <div className="min-h-[350px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
+            <h2 className="sm:text-4xl text-2xl font-bold mb-6">
+              Explore the World
+            </h2>
+            <p className="sm:text-lg text-base text-center text-gray-200">
+              Embark on unforgettable journeys. Book your dream vacation today!
+            </p>
+
+            <button
+              type="button"
+              className="mt-12 bg-transparent text-white text-base py-3 px-6 border border-white rounded-lg hover:bg-white hover:text-black transition duration-300"
+            >
+              Explore Now
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="max-w-5xl max-md:max-w-xl mx-auto font-[sans-serif] my-4">
+          <div className="text-center max-w-2xl mx-auto">
+            <h2 className="text-gray-800 text-3xl font-extrabold text-center mb-6">
+              Our Exclusive Features
+            </h2>
+            <p className="text-gray-600 text-sm">
+              Unlock a world of possibilities with our exclusive features.
+              Explore how our unique offerings can transform your journey and
+              empower you to achieve more.
+            </p>
+          </div>
+
+          <div className="mt-16">
+            <div className="grid md:grid-cols-2 items-center gap-16">
+              <div>
+                <Image
+                  alt=""
+                  src={backend}
+                  className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]"
+                />
+              </div>
+              <div>
+                <h3 className="text-gray-800 text-xl font-bold mb-4">
+                  Back End Development
+                </h3>
+                <p className="text-gray-600 text-sm">
+                  Qui elit labore in nisi dolore tempor anim laboris ipsum ad ad
+                  consequat id. Dolore et sint mollit in nisi tempor culpa
+                  consectetur. Qui elit labore in nisi dolore tempor anim
+                  laboris ipsum ad ad consequat id.
                 </p>
-            </main>
+                {/* <button
+                  type="button"
+                  className="px-6 py-2 mt-8 rounded text-white text-sm tracking-wider font-medium outline-none border-2 border-blue-600 bg-blue-600 hover:bg-transparent hover:text-black transition-all duration-300"
+                >
+                  Read More
+                </button> */}
+              </div>
+              <hr />
+              <div className="max-md:order-1">
+                <h3 className="text-gray-800 text-xl font-bold mb-4">
+                  Front End Development
+                </h3>
+                <p className="text-gray-600 text-sm">
+                  Qui elit labore in nisi dolore tempor anim laboris ipsum ad ad
+                  consequat id. Dolore et sint mollit in nisi tempor culpa
+                  consectetur. Qui elit labore in nisi dolore tempor anim
+                  laboris ipsum ad ad consequat id.
+                </p>
+                {/* <button
+                  type="button"
+                  className="px-6 py-2 mt-8 rounded text-white text-sm tracking-wider font-medium outline-none border-2 border-blue-600 bg-blue-600 hover:bg-transparent hover:text-black transition-all duration-300"
+                >
+                  Read More
+                </button> */}
+              </div>
+              <div>
+                <Image
+                  alt=""
+                  src={Frontend}
+                  className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    <hr />
+    <section>
+        <div className="max-w-5xl max-md:max-w-xl mx-auto font-[sans-serif] my-4">
+          <div className="mt-16">
+            <div className="grid md:grid-cols-2 items-center gap-16">
+             
+              <div className="max-md:order-1">
+                <h3 className="text-gray-800 text-xl font-bold mb-4">
+                 UI/UX Design
+                </h3>
+                <p className="text-gray-600 text-sm">
+                  Qui elit labore in nisi dolore tempor anim laboris ipsum ad ad
+                  consequat id. Dolore et sint mollit in nisi tempor culpa
+                  consectetur. Qui elit labore in nisi dolore tempor anim
+                  laboris ipsum ad ad consequat id.
+                </p>
+                {/* <button
+                  type="button"
+                  className="px-6 py-2 mt-8 rounded text-white text-sm tracking-wider font-medium outline-none border-2 border-blue-600 bg-blue-600 hover:bg-transparent hover:text-black transition-all duration-300"
+                >
+                  Read More
+                </button> */}
+              </div>
+              <div>
+                <Image
+                  alt=""
+                  src={ui_ux}
+                  className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-            <section className="grid lg:grid-cols-2">
-                <div className="px-10 py-20">
-                    <h1>Frontend
-                        Development</h1>
-                    <p>Our Frontend engineer are ready to face any challenges and are comfortable working with latest technologies and frameworks, like JavaScript.</p>
-                </div>
-
-                <div>
-                    <Image src={Frontend} alt=""
-                        className="float-left px-10"
-                        height={500}
-                        width={500} />
-                </div>
-
-            </section>
-
-
-
-            <section className="grid lg:grid-cols-2">
-                <div>
-                    <Image src={backend} alt=""
-                        className="px-10"
-                        height={500}
-                        width={500} />
-                </div>
-
-                <div className="px-10 py-20">
-                    <h1>Frontend
-                        Development</h1>
-                    <p>Our Frontend engineer are ready to face any challenges and are comfortable working with latest technologies and frameworks, like JavaScript.</p>
-
-                </div>
-
-            </section>
-            <section className="grid lg:grid-cols-2">
-                <div className="px-10 py-20">
-                    <h1>Frontend
-                        Development</h1>
-                    <p>Our Frontend engineer are ready to face any challenges and are comfortable working with latest technologies and frameworks, like JavaScript.</p>
-                </div>
-
-                <div>
-                    <Image src={ui_ux} alt=""
-                        className="float-left px-10"
-                        height={500}
-                        width={500} />
-                </div>
-
-            </section>
-
-        </>
-    )
+    </>
+  );
 }
