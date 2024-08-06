5ee62b145c59cf16ea2b957016ba0adeda26d059
chandankumar1307 <chandan.kumar@elitemindz.co>
2024-08-06 13:13:07 +0530

    ui changes

--- src/app/ar_vr_development/page.tsx
+++ src/app/ar_vr_development/page.tsx
@@ -1,11 +1,57 @@
-"use client"
-import OurValuePage from "@/components/home_page_content/our_value";
-import Image from "next/image"
-import "./web.css";
+"use client";
+const arvr = require("../../../public/ArVr.png");
 export default function ArVrDevelopmentPage() {
-    return (
-        <>
-          
-        </>
-    )
+  return (
+    <>
+      <div className="bg-[#182b50] px-8 py-16 font-[sans-serif]">
+        <div className="max-w-6xl mx-auto grid md:grid-cols-2 justify-center items-center gap-12">
+          <div className="text-center md:text-left">
+            <h2 className="text-4xl lg:text-5xl font-extrabold text-white mb-6 md:!leading-[55px]">
+              Immersive Experiences with AR/VR Solutions by-
+            </h2>
+            <p className="text-lg lg:text-xl text-white">
+              Creating Immersive Experiences with AR and VR Technology
+            </p>
+            <a
+              href="jacascrip:void(0);"
+              className="mt-12 bg-[#a91079] hover:bg-opacity-80 text-white py-3 px-6 rounded-full text-lg lg:text-xl transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl inline-block"
+            >
+              Explore Now
+            </a>
+          </div>
+          <div className="text-center">
+            <img src={arvr} alt="Premium Benefits" className="w-full mx-auto" />
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-50 w-full rounded font-[sans-serif] overflow-hidden">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 items-center">
+          <div className="lg:col-span-2 py-10 px-6">
+            <h1 className="text-gray-800 text-3xl font-bold">AR-VR Solution</h1>
+            <p className="mt-4 text-sm text-gray-500">
+              At---- Solutions, we are dedicated to transforming the way
+              businesses engage with their customers and employees through the
+              power of Augmented Reality (AR) and Virtual Reality (VR). Our
+              mission is to provide state-of-the-art AR/VR solutions that create
+              immersive, interactive, and impactful experiences. Partner with us
+              to bring your vision to life and revolutionize your industry with
+              cutting-edge AR/VR technology.
+            </p>
+            <button
+              type="button"
+              className="px-5 py-3 mt-8 rounded text-white text-sm tracking-wider font-semibold border-none outline-none bg-blue-600 hover:bg-blue-700"
+            >
+              Get the Free Guide
+            </button>
+          </div>
+          <img
+            src="https://readymadeui.com/cardImg.webp"
+            className="w-full h-full object-cover shrink-0"
+          />
+        </div>
+      </div>
+
+    </>
+  );
 }
--- src/app/finance_banking/page.tsx
+++ src/app/finance_banking/page.tsx
@@ -1,123 +1,189 @@
-"use client"
-import Image from "next/image"
-const Frontend = require("../../../public/frontEnd.png")
-const backend = require("../../../public/backend.png")
-const ui_ux = require("../../../public/UIUx.jpg")
+"use client";
 
 export default function FinancePage() {
-    return (
-        <>
-            <main >
-                <div className="font-sans bg-white py-12 px-4 bg-gradient-to-r from-white via-[#E4FE66] to-[#55F5A3]">
-                    <div className="max-w-7xl max-md:max-w-md mx-auto max-md:text-center">
-                        <h2 className="text-gray-800 lg:text-6xl md:text-5xl text-3xl font-extrabold lg:!leading-[64px] mt-10 md:max-w-4xl">Driving Innovation in Finance and Banking</h2>
-                        <div className="grid md:grid-cols-2 gap-12 mt-6">
-                            <div>
-                                <p className="text-gray-600 text-base leading-relaxed">
-                                    At our Solutions, we are dedicated to transforming the finance and banking sector with cutting-edge 
-                                    software solutions. Our mission is to provide secure, efficient, and user-friendly technologies that
-                                     drive innovation, streamline operations, and enhance customer experiences. Trust us to deliver the tools
-                                      you need to stay ahead in the fast-paced world of finance.
-                                </p>
-                            </div>
-                            <div>
-                                <img src='https://readymadeui.com/readymadeui_banner.webp' className="shrink-0 w-full h-full rounded-md object-contain" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
+  return (
+    <>
+      <main>
+        <div className="font-sans bg-white py-12 px-4 bg-gradient-to-r from-white via-[#E4FE66] to-[#55F5A3]">
+          <div className="max-w-7xl max-md:max-w-md mx-auto max-md:text-center">
+            <h2 className="text-gray-800 lg:text-6xl md:text-5xl text-3xl font-extrabold lg:!leading-[64px] mt-10 md:max-w-4xl">
+              Driving Innovation in Finance and Banking
+            </h2>
+            <div className="grid md:grid-cols-2 gap-12 mt-6">
+              <div>
+                <p className="text-gray-600 text-base leading-relaxed">
+                  At our Solutions, we are dedicated to transforming the finance
+                  and banking sector with cutting-edge software solutions. Our
+                  mission is to provide secure, efficient, and user-friendly
+                  technologies that drive innovation, streamline operations, and
+                  enhance customer experiences. Trust us to deliver the tools
+                  you need to stay ahead in the fast-paced world of finance.
+                </p>
+              </div>
+              <div>
+                <img
+                  src="https://readymadeui.com/readymadeui_banner.webp"
+                  className="shrink-0 w-full h-full rounded-md object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
 
-            <section className="">
+      <section className="">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 px-4 my-12">
           <div className="bg-gray-100 p-6 rounded-md">
-
-            <h3 className="text-lg font-bold mb-2 text-gray-800">E-Learning Solutions</h3>
+            <h3 className="text-lg font-bold mb-2 text-gray-800">
+              E-Learning Solutions
+            </h3>
             <p className="text-sm text-gray-600">
-              Our e-learning solutions provide flexible and accessible learning opportunities for 
-              students of all ages. We develop interactive e-learning platforms, digital course
-               materials, and assessment tools that enable remote learning and support a 
-               variety of learning styles.
+              Our e-learning solutions provide flexible and accessible learning
+              opportunities for students of all ages. We develop interactive
+              e-learning platforms, digital course materials, and assessment
+              tools that enable remote learning and support a variety of
+              learning styles.
             </p>
           </div>
           <div className="bg-gray-100 p-6 rounded-md">
-
-            <h3 className="text-lg font-bold mb-2 text-gray-800">Educational Content Management</h3>
+            <h3 className="text-lg font-bold mb-2 text-gray-800">
+              Educational Content Management
+            </h3>
             <p className="text-sm text-gray-600">
-            Efficiently manage and deliver educational content with our content management
-             solutions. We provide tools for creating, organizing, and distributing digital 
-             content, ensuring that educators can easily 
-            share resources and students can access the materials they need.
+              Efficiently manage and deliver educational content with our
+              content management solutions. We provide tools for creating,
+              organizing, and distributing digital content, ensuring that
+              educators can easily share resources and students can access the
+              materials they need.
             </p>
           </div>
           <div className="bg-gray-100 p-6 rounded-md">
-
-            <h3 className="text-lg font-bold mb-2 text-gray-800">Collaborative Learning Platforms</h3>
-            <p className="text-sm text-gray-600">Foster collaboration and communication 
-              among students and educators with our collaborative learning platforms. 
-              These tools include discussion forums, group projects, peer assessments, 
-              and real-time communication features, 
-              creating a dynamic and interactive learning environment</p>
-           </div>
-
+            <h3 className="text-lg font-bold mb-2 text-gray-800">
+              Collaborative Learning Platforms
+            </h3>
+            <p className="text-sm text-gray-600">
+              Foster collaboration and communication among students and
+              educators with our collaborative learning platforms. These tools
+              include discussion forums, group projects, peer assessments, and
+              real-time communication features, creating a dynamic and
+              interactive learning environment
+            </p>
+          </div>
         </div>
       </section>
 
       <div className="font-[sans-serif] text-gray-800 bg-gray-100 px-6 py-12">
-      <div className="grid lg:grid-cols-2 gap-8 max-lg:max-w-2xl mx-auto">
-        <div className="text-left">
-          <h2 className="text-4xl font-extrabold mb-6">Why Choose us?</h2>
-          <p className="mb-4 text-sm">
-          With extensive experience in the education sector,
-           we understand the unique needs and challenges of educational institutions.
-            Our team is dedicated to developing solutions that meet these needs and enhance
-             the overall learning experience.
-
-          </p>
-          <div className="grid xl:grid-cols-3 sm:grid-cols-2 gap-8 mt-12">
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" className="fill-green-500 shrink-0" viewBox="0 0 24 24">
-                <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
-              </svg>
-              <h6 className="text-base font-semibold ml-4">Innovation-Driven</h6>
-            </div>
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" className="fill-green-500 shrink-0" viewBox="0 0 24 24">
-                <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
-              </svg>
-              <h6 className="text-base font-semibold ml-4">User-Centric Design</h6>
-            </div>
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" className="fill-green-500 shrink-0" viewBox="0 0 24 24">
-                <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
-              </svg>
-              <h6 className="text-base font-semibold ml-4">Comprehensive Support</h6>
-            </div>
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" className="fill-green-500 shrink-0" viewBox="0 0 24 24">
-                <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
-              </svg>
-              <h6 className="text-base font-semibold ml-4">Performance</h6>
-            </div>
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" className="fill-green-500 shrink-0" viewBox="0 0 24 24">
-                <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
-              </svg>
-              <h6 className="text-base font-semibold ml-4">Global Reach</h6>
-            </div>
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" className="fill-green-500 shrink-0" viewBox="0 0 24 24">
-                <path d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z" data-original="#000000"></path>
-              </svg>
-              <h6 className="text-base font-semibold ml-4">Communication</h6>
+        <div className="grid lg:grid-cols-2 gap-8 max-lg:max-w-2xl mx-auto">
+          <div className="text-left">
+            <h2 className="text-4xl font-extrabold mb-6">Why Choose us?</h2>
+            <p className="mb-4 text-sm">
+              With extensive experience in the education sector, we understand
+              the unique needs and challenges of educational institutions. Our
+              team is dedicated to developing solutions that meet these needs
+              and enhance the overall learning experience.
+            </p>
+            <div className="grid xl:grid-cols-3 sm:grid-cols-2 gap-8 mt-12">
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  className="fill-green-500 shrink-0"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <h6 className="text-base font-semibold ml-4">
+                  Innovation-Driven
+                </h6>
+              </div>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  className="fill-green-500 shrink-0"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <h6 className="text-base font-semibold ml-4">
+                  User-Centric Design
+                </h6>
+              </div>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  className="fill-green-500 shrink-0"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <h6 className="text-base font-semibold ml-4">
+                  Comprehensive Support
+                </h6>
+              </div>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  className="fill-green-500 shrink-0"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <h6 className="text-base font-semibold ml-4">Performance</h6>
+              </div>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  className="fill-green-500 shrink-0"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <h6 className="text-base font-semibold ml-4">Global Reach</h6>
+              </div>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  className="fill-green-500 shrink-0"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <h6 className="text-base font-semibold ml-4">Communication</h6>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <img src="https://readymadeui.com/management-img.webp" alt="Placeholder Image" className="rounded-lg object-cover w-full h-full" />
+          <div className="flex justify-center items-center">
+            <img
+              src="https://readymadeui.com/management-img.webp"
+              alt="Placeholder Image"
+              className="rounded-lg object-cover w-full h-full"
+            />
+          </div>
         </div>
       </div>
-    </div>
-        </>
-    )
+    </>
+  );
 }
